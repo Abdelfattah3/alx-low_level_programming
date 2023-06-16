5c0c10f659af2365cf56cb5e72f2daaacfa683b2
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-16 07:22:21 -0700

    new

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point of the program.
+ *This function prints my age.
+ *Return: Always 0 (success).
+ */
+int main(void)
+{
+	int age = 25;/* age */
+
+	printf("My age is %d\n", age);
+	return (0);
+	}
