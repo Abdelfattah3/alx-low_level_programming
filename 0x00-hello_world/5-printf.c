c31fa4c966ef809a65bbc05f45436814825e806b
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-16 10:45:36 -0700

    sd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry start
+ * the function is to print a sentence
+ * Return: 0 successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
