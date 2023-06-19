d00e7a4786afe1e4d880df3803f140404ab09c32
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-19 09:04:22 -0700

    df

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints hexidecimal
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 102)
+	{
+		putchar(x);
+		if (x == 57)
+			x += 39;
+		x++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
