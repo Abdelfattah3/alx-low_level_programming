9368eb2fc869ddf3de4f5034f30c0e58f4d6a30a
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-19 08:39:28 -0700

    df

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints decimal numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar (a + '0');
+	}
+	putchar ('\n');
+	return (0);
+}
