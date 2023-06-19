a881c5be987c342798358314e4687bb7972f83f5
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-19 08:23:27 -0700

    df

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints decimal numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
