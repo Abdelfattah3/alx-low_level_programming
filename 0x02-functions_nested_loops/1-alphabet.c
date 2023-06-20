5255908232ad38daab6fc5a76114246d851621eb
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-20 11:27:40 -0700

    df

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - entry
+* Description : hlee
+* Return: 0 (success)
+*/
+void print_alphabet(void);
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z' ; ch++);
+		_putchar(ch);
+	_putchar('\n')
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+
+void print_alphabet(void);
+
+_putchar(char c);
 
 #endif
