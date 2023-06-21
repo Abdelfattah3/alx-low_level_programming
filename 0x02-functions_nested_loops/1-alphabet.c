1a13aa28d5c90aa90c85843f8a62cf54aeb81ec3
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-21 09:56:26 -0700

    mjh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * main - entry
 * Description : hlee
 * Return: 0 (success)
 */
-void print_alphabet(void);
 
 int main(void)
 {
@@ -13,4 +13,5 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z' ; ch++);
 		_putchar(ch);
-	_putchar('\n')
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,6 @@
 
 #include <stdio.h>
 
-void print_alphabet(void);
-
 _putchar(char c);
 
 #endif
