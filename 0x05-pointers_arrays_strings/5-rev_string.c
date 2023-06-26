b89faa5c56f682f3cc86bb544f331882e4b71387
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-26 07:29:07 -0700

    df

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* rev_string - swaps the values of two integers
+*
+* @s: input paramter
+*
+* Return: nothing
+*/
+
+void rev_string(char *s)
+{
+	int l,i;
+	char temp;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < l /2; i++)
+	{
+		temp = s[i];
+		s[i] = s[l - 1 -i];
+		s[l - 1 - i] = temp;
+	}
+}
