07066d700cef70f688e7bd537f3fe9fbd6c0eb88
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-28 04:59:59 -0700

    df

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _abs - prints alphabets in order
+* @n: input character
+* Return: (0) success
+*/
+
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
