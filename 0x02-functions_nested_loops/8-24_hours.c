3d1c8aaea5aa50293b662c16d586c0fb4945b318
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-28 05:29:09 -0700

    df

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* jack_bauer - prints alphabets in order
+* @n: input character
+* Return: (0) success
+*/
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0;min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
