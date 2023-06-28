ba2eb8030046f2e14c2ce4e2a1c66b2a5ef55396
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-28 06:58:52 -0700

    sd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,32 +3,31 @@
 
 /**
 * times_table - prints alphabets in order
-* Return: (0) success
+* Example table
+* 0, 0, 0, 0, ,,
+* 0, 1, 2, 3, ,,
+*
 */
 
 void times_table(void)
 {
-	int a, b, c;
+	int num, mult, prod;
 
-	for (a = 0; a <= 9; a++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
-		for (b = 1; b <= 9; b++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
-			c = a * b;
-			_putchar(c);
-			if (c <= 9)
-			{
+			prod = num * mult;
+			if (prod <= 9)
 				_putchar(' ');
-			}
 			else
-			{
-				_putchar((c / 10) + 48);
-				_putchar((c % 10) + 48);
-			}
-		}_putchar('\n');
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
 	}
 }
