d5188b259bc87dd6008bdf6d41011238b913acbc
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-28 09:01:09 -0700

    sd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,22 +16,26 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
+		_putchar(',');
+		_putchar(' ');
 		for (mult = 1; mult <= 9; mult++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			prod = num * mult;
 
-			if (prod <= 9)
+			if ((prod / 10) > 0)
 			{
-				_putchar(' ');
+				_putchar((prod / 10) + '0');
 			}
 			else
 			{
-				_putchar((prod / 10) + 48);
-				_putchar((prod % 10) + 48); 
+				_putchar(' ');
 			}
+				_putchar((prod % 10) + 48); 
+				if (prod < 9)
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
 		}
 		_putchar('\n');
 	}
