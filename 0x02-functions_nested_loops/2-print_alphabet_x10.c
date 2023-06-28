53e63cf78cb0744da3fe98226d1721c27ef24628
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-28 02:36:09 -0700

    df

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_alphabet_x10 - prints alphabets in order
+*/
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i = 0;
+
+	for (i = 0; i >= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	}
+}
