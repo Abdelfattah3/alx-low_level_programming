b8c91bc40b64244e400935c5f006f1ae041fd262
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-29 08:36:21 -0700

    df

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,7 +3,10 @@
 
 /**
 * infinite_add - Concatenates two strings
-* @n: string 1
+* @n1: string 1
+* @n2: string1 2
+* @r: string
+* @size_r: string of size
 * Return: 1 if true 0 if false
 */
 
@@ -16,38 +19,24 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (n2[j] != '\0')
 		j++;
 	if (i < j)
-	{
 		l = i;
-	}
 	else
-	{
 		l = j;
-	}
 	if (l + 1 > size_r)
-	{
 		return (0);
-	}
 	r[l] = '\0';
 	for (k = l - 1; k >= 0; k--)
 	{
 		i--;
 		j--;
-		if ( i >= 0)
-		{
+		if (i >= 0)
 			f = n1[i] - '0';
-		}
 		else
-		{
 			f = 0;
-		}
 		if (j >= 0)
-		{
 			s = n2[j] - '0';
-		}
 		else
-		{
 			s = 0;
-		}
 		r[k] = (f + s + d) % 10 + '0';
 		d = (f + s + d) / 10;
 	}
@@ -55,9 +44,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	{
 		r[l + 1] = '\0';
 		if (l + 2 > size_r)
-		{
 			return (0);
-		}
 		while (l-- >= 0)
 			r[l + 1] = r[l];
 		r[0] = d + '0';
