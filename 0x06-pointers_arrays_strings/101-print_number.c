908a274550abc0603e5f0b0daa69a59ba5986980
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-29 08:01:40 -0700

    df

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - Concatenates two strings
+* @n: string 1
+* Return: 1 if true 0 if false
+*/
+
+void print_number(int n)
+{
+	unsigned int n1;
+	
+	n1 = n;
+	
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
