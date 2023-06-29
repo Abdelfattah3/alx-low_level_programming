8ba317334a5e605f9c5ba77dc474f80adb26acba
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-29 07:12:19 -0700

    fd

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* string_toupper - Concatenates two strings
+* @str: string 1
+* Return: 1 if true 0 if false
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0, str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str)
+}
