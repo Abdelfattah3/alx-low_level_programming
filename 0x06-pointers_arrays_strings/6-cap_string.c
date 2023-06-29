45452641c14694efa7de61b5364a6311de018ae8
Abdelfattah3 <abdoselkady456@gmail.com>
2023-06-29 07:32:24 -0700

    d

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -46,7 +46,7 @@ char *cap_string(char *s)
 	{
 		if (isDelimiter(*s))
 			foundDelimit = 1;
-		else if (islower(*s) && foundDelimit)
+		else if (isLower(*s) && foundDelimit)
 		{
 			*s -=32;
 			foundDelimit = 0;
