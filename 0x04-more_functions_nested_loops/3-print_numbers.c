6f892a72bc86c46d51a848493c910a186164e780
Abdelfattah3 <abdoselkady456@gmail.com>
2023-07-11 03:14:28 -0700

    d

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - checks if the alphabet is uppercase or not
+* Return: 1 if uppercase 0 if lowercase
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	_putchar(i);
+	_putchar('\n');
+
+}
+
