6c5ce969f1507300f93b5d88a89374db86e4fc29
Abdelfattah3 <abdoselkady456@gmail.com>
2023-07-11 03:37:46 -0700

    df

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* more_numbers - checks if the alphabet is uppercase or not
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = '0'; i <= '14'; i++)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
+
