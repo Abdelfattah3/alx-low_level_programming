68f982f46d3ebde3754b3820263ce5712a68f808
Abdelfattah3 <abdoselkady456@gmail.com>
2023-07-11 03:07:12 -0700

    sd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* mul - checks if the alphabet is uppercase or not
+* @a: input character
+* @b: input character
+* Return: 1 if uppercase 0 if lowercase
+*/
+
+int mul(int a, int b)
+{
+	int res;
+	
+	res = a / b;
+	return (res);
+}
+
