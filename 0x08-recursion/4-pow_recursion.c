d030bceeec64352ad6571045f6e91444a5f3a22e
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-02 07:15:12 -0700

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _pow_recursion - prints string
+* @x : int
+* @y : int
+* Return: int
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recusrion(x, y - 1));
+}
