919f10a7cda6ebb5cc96c66fa67c6f8535927508
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-02 07:39:42 -0700

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+* _sqrt_recursion - return the square number
+* check - checks for the number square number
+* @n : the number
+* @m : int
+* Return: an int
+*/
+
+int check(int n, int m);
+int _sqrt_recursion(int n)
+{
+	return (check(n, 1));
+}
+
+/**
+* check - checks for the number square number
+* @n : int
+* @m : int
+* Return: an int
+*/
+
+int check(int n, int m)
+{
+	if (n == m * m)
+		return (m);
+	else if (n > m)
+		check(n, m + 1);
+	else
+		return (-1);
+}
+
