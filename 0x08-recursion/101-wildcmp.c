64b9be3e6223197bef50d67355a965ee67110baa
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-02 17:11:30 -0700

    9

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,8 +18,10 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	if (*s1 != *s2 && *s2 == '*')
 		return (wildcmp(s1, s2 + 1));
-	if (*(s2 - 1) == '*' && *s1 != *s2 && *(s2+1) != '\0')
+	if (*(s2 - 1) == '*' && *s1 != *s2)
 		return (wildcmp(s1 + 1, s2));
+	if (*(s2 + 1 ) == '\0')
+		return (1);
 	if (*s1 == *s2 || ((*s2 == '*') && (*s1 != '\0')))
 		return (wildcmp(s1 + 1, s2 + 1));
 	else
