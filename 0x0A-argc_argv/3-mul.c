158f45cc5cfba603ecaaccdae92dfb0c1f614efb
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-04 07:45:51 -0700

    4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,13 +10,16 @@ int main(int argc, char **argv)
 {
 	int n;
 
-	if (argc < 2)
+	if (argc == 3)
+	{
+		n = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", n);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (argc == 3)
-	n = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", n);
 	return (0);
+
 }
