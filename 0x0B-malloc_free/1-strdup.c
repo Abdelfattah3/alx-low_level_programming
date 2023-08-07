bdedcc63fec23c6e2d7f03320bbd23c52aa2ecc9
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-07 04:24:13 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* stt - calculate the string length
+* @s : string
+* Return: int
+*/
+int stt(char *s)
+{
+	int i = 1;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
+* _strdup - creates an array
+* @str : string
+* Return: pointer
+*/
+char *_strdup(char *str)
+{
+	int j = stt(str);
+	int i;
+	char *a;
+
+	a = malloc(j * sizeof(char));
+	if (a == 0 || j == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= j; i++)
+	{
+		a[i] = str[i];
+	}
+	return (a);
+}
