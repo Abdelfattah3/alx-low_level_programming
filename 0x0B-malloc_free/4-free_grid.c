db1180a943d87701cfdb46a3b76f7f91fa849649
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-07 09:12:50 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* free_grid - creates an array
+* @grid : double poniter to int
+* @height : int
+* Return: void
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+}
