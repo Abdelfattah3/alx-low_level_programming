78dc5e9051dd228cee4de19990d7ae2e8465ec97
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-14 03:48:56 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+* struct dog - structre for a dog data
+* @name : dog name
+* @age : dog age
+* @owner : dog owner
+*
+* Description: this struct contains dogs data
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
