41479af983e95884fa0342c6d1aefde275ce9046
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-14 06:56:01 -0700

    task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,24 +18,14 @@ int _strlen(char *n)
 	}
 	return (length);
 }
-char *copy(char *name)
+char *copy(char *m, char *name)
 {
-	char *a;
-	char *b;
-
-	a = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (a == NULL)
-	{
-		free(a);
-		return (NULL);
-	}
-	b = a;
 	while (*name != '\0')
 	{
-		*a++ = *name++;
+		*m++ = *name++;
 	}
-	*a = '\0';
-	return (b);
+	*m = '\0';
+	return (m);
 }
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -51,20 +41,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(mydog);
 		return (NULL);
 	}
-	mydog->name = malloc(sizeof(char) * _strlen(name));
+	mydog->name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (mydog->name == NULL)
 	{
 		free(mydog->name);
 		return (NULL);
 	}
-	mydog->owner = malloc(sizeof(char) * _strlen(owner));
+	mydog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (mydog->owner == NULL)
 	{
 		free(mydog->owner);
 		return (NULL);
 	}
-	(*mydog).name = copy(name);
+	(*mydog).name = copy(mydog->name, name);
 	(*mydog).age = age;
-	(*mydog).owner = copy(owner);
+	(*mydog).owner = copy(mydog->owner, owner);
 	return (mydog);
 }
