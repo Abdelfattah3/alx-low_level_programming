ef6ec1e570011a4ff2e06555e980b8c87c2a5e99
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-14 06:17:07 -0700

    task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include "string.h"
 /**
 * new_dog - create a structure pointer
 * @name : dog name
@@ -8,26 +7,58 @@
 * @owner : dog owner
 * Return: pointer to the new structure
 */
+int _strlen(char *name)
+{
+	int length = 0;
+
+	while (!*name)
+	{
+		length++;
+		name++;
+	}
+	return (length);
+}
+char *copy(char *name)
+{
+	char *a;
+
+	while (!*name)
+	{
+		*a++ = *name++;
+	}
+	*a = '\0';
+	return (a);
+}
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *mydog;
 	char *a;
 	char *b;
 
-	b = malloc(sizeof(char) * strlen(owner));
-	a = malloc(sizeof(char) * strlen(name));
-	strncpy(a, name, strlen(name));
-	strncpy(b, owner, strlen(owner));
-	mydog = malloc(sizeof(dog_t));
-	if (mydog == NULL)
+	if (name == NULL || owner == NULL || age < 0)
 	{
 		return (NULL);
 	}
-	if (name == NULL || owner == NULL)
+	mydog = malloc(sizeof(dog_t));
+	if (mydog == NULL)
 	{
-		name = "";
-		owner = "";
+		free(mydog);
+		return (NULL);
 	}
+	a = malloc(sizeof(char) * (_strlen(name + 1)));
+	if ( a == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
+	a = copy(name);
+	b = malloc(sizeof(char) * (_strlen(owner + 1)));
+	if ( b == NULL)
+	{
+		free(b);
+		return (NULL);
+	}
+	b = copy(owner);
 	(*mydog).name = name;
 	(*mydog).age = age;
 	(*mydog).owner = owner;
