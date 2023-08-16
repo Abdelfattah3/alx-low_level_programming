c1df620b16b58ecd0adf0b4215f6e0526b87d59e
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-16 09:46:17 -0700

    task 4

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "calc.h"
+#include <stdlib.h>
+/**
+* main - do operations
+* @argc : int
+* @argv : string
+* Return: 0 if successful
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = get_op_func(argv[2])(a, b);
+	printf("%d\n", c);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+/**
+* op_add - addition operation
+* @a : int
+* @b : int
+* Return: an int
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - substraction operation
+* @a : int
+* @b : int
+* Return: an int
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - multiplication operation
+* @a : int
+* @b : int
+* Return: an int
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - division operation
+* @a : int
+* @b : int
+* Return: an int
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - modulation operation
+* @a : int
+* @b : int
+* Return: an int
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
