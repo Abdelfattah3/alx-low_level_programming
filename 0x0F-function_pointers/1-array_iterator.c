02b693a09ad6a656ba5975a564dcc1a5b2d21052
Abdelfattah3 <abdoselkady@gmail.com>
2023-08-16 04:54:47 -0700

    task 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+* array_iterator - function pointer to print a name
+* @array : pointer to an int
+* @size :  size of an array
+* @action : function pointer
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	i = 0;
+	if (action != NULL)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+
+	}
+}
