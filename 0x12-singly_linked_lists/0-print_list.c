7f0c46988bbd70b6e2520689c83626c01b159060
Abdelfattah3 <abdoselkady456@gmail.com>
2023-08-25 09:01:35 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+* print_list - prints the elemnets of a list
+* @h : pointer to the head of the list
+* Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	if (h == NULL)
+	{
+		return (-1);
+	}
+	if (h->next == NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		return (1);
+	}
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	return (1 + print_list(h->next));
+}
