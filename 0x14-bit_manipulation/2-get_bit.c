eb2e8835a2081e38d6095a257774af08e84b965b
Abdelfattah3 <abdoselkady456@gmail.com>
2023-09-01 09:51:44 -0700

    task 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+* count_binary - prints the binary representation of a number
+* @n : the number
+* Return: the count of the binary bits
+*/
+unsigned int count_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (1 + count_binary(n >> 1));
+}
+/**
 * get_bit - gets the value of a bit at a given index
 * @n : the number
 * @index : the index number
@@ -7,6 +20,12 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int i = count_binary(n);
+
+	if (index > i)
+	{
+		return (-1);
+	}
 	if ((n >> index) & 1)
 	{
 		return (1);
@@ -14,6 +33,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	else
 	{
 		return (0);
-	}	
-	return (-1);
+	}
 }
