7504cf81eedfe0701ab2c7b5ac315170841d35a0
Abdelfattah3 <abdoselkady456@gmail.com>
2023-09-01 10:00:52 -0700

    task 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -24,14 +24,17 @@ int get_bit(unsigned long int n, unsigned int index)
 
 	if (index > i)
 	{
-		return (-1);
-	}
-	if ((n >> index) & 1)
-	{
-		return (1);
+		exit(-1);
 	}
 	else
 	{
-		return (0);
+		if ((n >> index) & 1)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 	}
 }
