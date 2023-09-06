4e5029450e870324b01d13229a5b63ef03fde6cd
Abdelfattah3 <abdoselkady456@gmail.com>
2023-09-06 11:34:05 -0700

    task 5

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <elf.h>
+/**
+ * main - ELF
+ * @argc: number of arguments.
+ * @argv: arguments values.
+ * Return: success (0)
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		exit(98);
+	}
+	return (0);
+}
