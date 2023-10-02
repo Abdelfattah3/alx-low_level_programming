cb87529468a2be60dee01ba21346867f1d84949a
Abdelfattah3 <abdoselady@gmail.com>
2023-10-02 10:16:09 -0700

    task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,21 +7,18 @@
  */
 int main(int ac, char **av)
 {
+	struct stat st;
 	char buf[1024];
 	int fd1, fd2;
 	ssize_t r;
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (stat(av[2], &st) == 0)
+		fd2 = open(av[2], O_WRONLY | O_TRUNC);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", av[2]), exit(99);
